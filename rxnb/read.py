1c9f188308947dc0ba8e782ce8264e92070afa6b
echo <licheng_xu@zju.edu.cn>
2024-08-02 14:46:36 +0800

    add read_xyz

--- rxnb/read.py
+++ rxnb/read.py
@@ -1,1 +1,9 @@
 from ase.io import read as ase_read
+def read_xyz(file_path):
+    with open(file_path,'r') as fr:
+        lines = fr.readlines()
+    atom_num = int(lines[0].strip())
+    symbol_coords_lines = lines[2:2+atom_num]
+    symbols = [line.split()[0] for line in symbol_coords_lines]
+    coords = [list(map(float,line.split()[1:4])) for line in symbol_coords_lines]
+    return symbols,coords
